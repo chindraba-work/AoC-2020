c26d255cf92a33b86f9fbbe64c1c4eea721f360c
Ronald Lamoreaux <gitkey@chindraba.work>
2020-12-04 23:48:33 +0000

    Clean up the code for potential future use
    The future challenges could be in a similar set of miss matched lines. It seems
    wise to be ready for it next time.
    
    Signed-off-by: Ronald Lamoreaux <gitkey@chindraba.work>

--- Solutions/day_04.pl
+++ Solutions/day_04.pl
@@ -49,22 +49,25 @@ my $result;
 
 my $testing = 0;
 my @check_data =(
+'ecl:gry pid:860033327 eyr:2020 hcl:#fffffd',
+'byr:1937 iyr:2017 cid:147 hgt:183cm',
+'',
+'iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884',
+'hcl:#cfa07d byr:1929',
+'',
+'hcl:#ae17e1 iyr:2013',
+'eyr:2024',
+'ecl:brn pid:760753108 byr:1931',
+'hgt:179cm',
+'',
+'hcl:#cfa07d eyr:2025 pid:166559648',
+'iyr:2011 ecl:brn hgt:59in',
 );
 
-my @puzzle_data = $testing ? @check_data : read_lines($puzzle_data_file);
+my @passport_list = map {
+    { map { split /:/, $_ } (split / /, $_) } 
+} ($testing ? split("  ", join(' ', @check_data)) : read_comma_list($puzzle_data_file, "  "));
 
-sub normalize {
-    my $set = join ',', @_;
-    $set =~ s/,,/\n/g;
-    $set =~ s/,/ /g;
-    return split /\n/, $set;
-}
-sub passport_record {
-    my %passport_data = map { split /:/, $_ } (split / /, $_[0]);
-}
-
-my @passport_list = map { {passport_record $_} } (normalize @puzzle_data);
-    
 # Part 1
 $part_num = 1;
 $result = 0;
@@ -101,12 +104,9 @@ map {
         defined $_->{'eyr'} && $_->{'eyr'} ~~ [2020 .. 2030] &&
         defined $_->{'hgt'} && $_->{'hgt'} =~ /^([0-9]+)(in|cm)$/ &&
             ( $2 eq 'cm' && $1 ~~ [150 .. 193] || $2 eq 'in' && $1 ~~ [59 .. 76] ) &&
-        defined $_->{'hcl'} &&
-            $_->{'hcl'} =~ /\#[0-9a-f]{6}/ &&
-        defined $_->{'ecl'} &&
-            $_->{'ecl'} =~ /amb|blu|brn|gry|grn|hzl|oth/ &&
-        defined $_->{'pid'} &&
-            $_->{'pid'} =~ /^[0-9]{9}$/
+        defined $_->{'hcl'} && $_->{'hcl'} =~ /\#[0-9a-f]{6}/ &&
+        defined $_->{'ecl'} && $_->{'ecl'} =~ /amb|blu|brn|gry|grn|hzl|oth/ &&
+        defined $_->{'pid'} && $_->{'pid'} =~ /^[0-9]{9}$/
     )
 } (@passport_list);
 
